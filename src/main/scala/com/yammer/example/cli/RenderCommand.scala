c617582e7443c24ca7e4518d7ca5e88e2af8800e
Coda Hale <coda.hale@gmail.com>
2011-03-17 17:32:26 -0700

    Update the CLI stuff.

--- src/main/scala/com/yammer/example/cli/RenderCommand.scala
+++ src/main/scala/com/yammer/example/cli/RenderCommand.scala
@@ -1,23 +1,17 @@
 package com.yammer.example.cli
 
-import com.yammer.dropwizard.cli.ConfiguredCommand
 import com.yammer.example.data.Template
-import org.apache.commons.cli.Options
+import com.yammer.dropwizard.cli.{Flag, ConfiguredCommand}
 
 class RenderCommand extends ConfiguredCommand {
-  override protected def commandSyntax(jarSyntax: String) =
-    "%s %s [options] <config file> [name1 name2]".format(jarSyntax, name)
-
+  def name = "render"
 
   override def description = Some("Renders the configured template to console")
 
-  override def cliOptions = {
-    val opts = new Options
-    opts.addOption("i", "include-default", false, "Also render the template with the default name")
-    Some(opts)
-  }
+  override protected def commandSyntax(jarSyntax: String) =
+    "%s %s [options] <config file> [name1 name2]".format(jarSyntax, name)
 
-  def name = "render"
+  override def options = Flag("i", "include-default", "Also render the template with the default name") :: Nil
 
   def runWithConfigFile(opts: Map[String, List[String]],
                         args: List[String]) = {
