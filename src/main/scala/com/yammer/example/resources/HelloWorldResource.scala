eb1d88d1f2966aa85df95280886f0e376d74fd6f
Coda Hale <coda.hale@gmail.com>
2011-03-24 19:39:18 -0700

    Instrumented HelloWorldResource.

--- src/main/scala/com/yammer/example/resources/HelloWorldResource.scala
+++ src/main/scala/com/yammer/example/resources/HelloWorldResource.scala
@@ -5,6 +5,7 @@ import com.google.inject.{Inject, Singleton}
 import javax.ws.rs.{QueryParam, GET, Produces, Path}
 import com.yammer.example.data.{Saying, Template}
 import java.util.concurrent.atomic.AtomicLong
+import com.yammer.metrics.guice.Timed
 
 @Path("/hello-world")
 @Produces(Array(MediaType.APPLICATION_JSON))
@@ -13,6 +14,7 @@ class HelloWorldResource @Inject()(template: Template) {
   private val counter = new AtomicLong(0)
 
   @GET
+  @Timed(name = "say-hello")
   def sayHello(@QueryParam("name") name: Option[String]) =
     Saying(counter.incrementAndGet, template(name))
 }
