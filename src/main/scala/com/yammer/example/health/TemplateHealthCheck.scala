8bd5c080a95385116e056804d552059ba8807a37
Coda Hale <coda.hale@gmail.com>
2011-03-25 16:27:59 -0700

    Make the health check a singleton.

--- src/main/scala/com/yammer/example/health/TemplateHealthCheck.scala
+++ src/main/scala/com/yammer/example/health/TemplateHealthCheck.scala
@@ -2,9 +2,10 @@ package com.yammer.example.health
 
 import com.yammer.metrics.core.HealthCheck
 import com.yammer.metrics.core.HealthCheck.Result
-import com.google.inject.Inject
 import com.yammer.example.data.Template
+import com.google.inject.{Singleton, Inject}
 
+@Singleton
 class TemplateHealthCheck @Inject()(template: Template) extends HealthCheck {
   def name = "template"
 
