984eeaab7d133f0da6eaef5ff3ee62aba69e5590
Coda Hale <coda.hale@gmail.com>
2011-05-13 13:04:16 -0700

    Now on DW 0.0.5

--- project/build/DropWizardExampleProject.scala
+++ project/build/DropWizardExampleProject.scala
@@ -24,7 +24,7 @@ class DropWizardExampleProject(info: ProjectInfo) extends DefaultProject(info)
   /**
    * Service Dependencies
    */
-  val dropWizard = "com.yammer" %% "dropwizard" % "0.0.5-SNAPSHOT"
+  val dropWizard = "com.yammer" %% "dropwizard" % "0.0.5"
 
   /**
    * Test Dependencies
